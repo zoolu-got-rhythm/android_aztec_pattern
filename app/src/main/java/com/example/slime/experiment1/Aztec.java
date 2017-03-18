13fb56c287d548d2957db43e5dbb84ec3eba0a0b
Christopher Morris <morriscp1@cardiff.ac.uk>
2017-03-18 01:29:04 +0000

    aztec pattern is functional but slow, can make it faster with some refactoring and optimization

--- app/src/main/java/com/example/slime/experiment1/Aztec.java
+++ app/src/main/java/com/example/slime/experiment1/Aztec.java
@@ -14,8 +14,9 @@ public class Aztec {
     private ArrayList<Coordinates> currentPattern;
     private Map<String, Coordinates> dir;
     private Coordinates prev;
-    // private Coordinates current;
     private int lines = 0;
+
+    private int i = 0;
 
     public Aztec(Coordinates origin){
         this.origin = origin;
@@ -26,54 +27,46 @@ public class Aztec {
         this.dir.put("s", new Coordinates(0, 1));
         this.dir.put("w", new Coordinates(-1, 0));
         this.prev = this.origin;
-        //this.currentPattern.add(new Coordinates(5, 2));
         this.currentPattern.add(this.origin);
     }
 
     public void move(){
-        if(lines % 2 == 0 && lines != 0)
-            length+=25;
-        //System.out.println(lines);
+        moveDirection();
 
-        for(int i = 0; i < length; i++){
-            moveDirection();
-        }
-
-        if(lines == 4){
-            lines = 0;
-        }else{
+        // i = to number of cubes in a line
+        if(i == length) {
             lines++;
+            if(lines % 2 == 0 && lines != 0){
+                length += 30;
+            }
+
+            if(lines == 4){
+                lines = 0;
+            }
+
+            i = 0;
         }
+
+        i++;
+
+        System.out.println(i);
     }
 
     public void moveDirection(){
-        // System.out.println("ran");
         switch (this.lines + 1){
             case 1: this.currentPattern.add(this.prev.plot(this.dir.get("n")));
-                    System.out.println("n");
-                    break;
+                break;
             case 2: this.currentPattern.add(this.prev.plot(this.dir.get("e")));
-                System.out.println("e");
-
                 break;
             case 3: this.currentPattern.add(this.prev.plot(this.dir.get("s")));
-                System.out.println("s");
-
                 break;
             case 4: this.currentPattern.add(this.prev.plot(this.dir.get("w")));
-                System.out.println("w");
-
                 break;
         }
 
         // get the last element and point prev reference to it in memory
         this.prev = this.getCurrentPattern().get(this.currentPattern.size() - 1);
 
-        System.out.println(this.prev.getX() + ", " + this.prev.getY());
-
-//        for(Coordinates co : this.getCurrentPattern()){
-//            System.out.println(co.getX() + "" + co.getY());
-//        }
     }
 
     public ArrayList<Coordinates> getCurrentPattern() {
--- app/src/main/java/com/example/slime/experiment1/CustomView.java
+++ app/src/main/java/com/example/slime/experiment1/CustomView.java
@@ -9,6 +9,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 /**
  * Created by Slime on 16/03/2017.
  */
@@ -19,11 +21,19 @@ class CustomView extends View {
     private float i = 0;
     private MySquare square;
     private Aztec aztec;
+    ArrayList<Integer> colors;
 
     public CustomView(Context context) {
         super(context);
         this.p = new Paint();
-        p.setColor(Color.BLACK);
+        // p.setColor(Color.BLACK);
+        this.colors = new ArrayList<>();
+        this.colors.add(Color.CYAN);
+        this.colors.add(Color.BLACK);
+        this.colors.add(Color.MAGENTA);
+        this.colors.add(Color.RED);
+
+        // super.getMeasuredWidth()
         this.aztec = new Aztec(new Coordinates(100, 100));
         this.square = new MySquare(50, 100, 150, 200);
         Toast.makeText(context, "asdf", Toast.LENGTH_LONG).show();
@@ -42,7 +52,9 @@ class CustomView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         invalidate();
+        setBackgroundColor(Color.BLACK);
         for(Coordinates co : this.aztec.getCurrentPattern()){
+            this.p.setColor(this.colors.get((int) Math.floor(Math.random() * colors.size())));
             canvas.drawRect(co.getY() + 20, co.getX() + 20, co.getY() + 40, co.getX() + 40, this.p);
         }
         //canvas.drawColor(Color.BLACK);
